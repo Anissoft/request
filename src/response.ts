9b151d1c72988dbe2bacbcd81d2f330101fd4c81
Alexey Anisimov <anissoftkun@gmail.com>
2020-03-18 15:23:13 +0700

    added fail action

--- src/response.ts
+++ src/response.ts
@@ -38,26 +38,31 @@ export class $Response<T, K> {
 
   public flush = () => {
     if (this.init?.actions) {
-      const reaction = Object.entries(this.init?.actions).find(([key, action]) => {
-        if (key === 'default') {
-          return false;
-        }
-        if (key === 'ok') {
-          return this.candidate.ok;
-        }
-        if (typeof key === 'string' && /\d{3}\-\d{3}/) {
-          const [start, end] = key.split('-');
-          return this.candidate.status >= +start && this.candidate.status <= +end;
-        }
-        return this.candidate.status === +key;
-      });
-      if (reaction) {
-        return reaction[1](this.candidate);
+      const [key, action] =
+        Object.entries(this.init?.actions).find(([key]) => {
+          if (key === 'default') {
+            return false;
+          }
+          if (key === 'ok') {
+            return !!this.candidate.ok;
+          }
+          if (key === 'fail') {
+            return !this.candidate.ok;
+          }
+          if (typeof key === 'string' && /\d{3}\-\d{3}/) {
+            const [start, end] = key.split('-');
+            return this.candidate.status >= +start && this.candidate.status <= +end;
+          }
+          return this.candidate.status === +key;
+        }) || [];
+      if (action) {
+        return action(this.candidate);
       }
 
-      const defaultReaction = Object.entries(this.init?.actions).find(([key]) => key === 'default');
-      if (defaultReaction) {
-        return defaultReaction[1](this.candidate);
+      const [, defautAction] =
+        Object.entries(this.init?.actions).find(([key]) => key === 'default') || [];
+      if (defautAction) {
+        return defautAction(this.candidate);
       }
     }
     if (this.init?.shouldThrow && !this.candidate.ok) {
