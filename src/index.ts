791cc9893cb5a85f6e80612822d9c46a1e77ae23
Anissoft <anissoftkun@gmail.com>
2020-08-18 20:38:03 +0700

    get rid of global variables

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,24 @@ import { InitializeOptions, ResponseExtra, RequestInitExtra } from './types';
 import { $Request } from './request';
 import { $Response } from './response';
 
-let __sendRequest: typeof fetch;
-let headers: Record<string, string> = {};
-
 export const initialize = (original: typeof fetch, options: InitializeOptions = {}) => {
-  __sendRequest = original;
-  if (options.headers) {
-    headers = options.headers;
+  const request = async <T = any, K = any>(
+    input: RequestInfo,
+    init: RequestInitExtra<T, K> = {},
+  ): Promise<ResponseExtra<T> | K> => {
+    const requsetExtra = new $Request(input, init, original);
+    await requsetExtra.setHeaders(options.headers || {});
+    await requsetExtra.parseBody();
+
+    const responseExtra = new $Response(await requsetExtra.flush(), init);
+    await responseExtra.parseBody();
+    await responseExtra.defineOk();
+
+    return responseExtra.flush();
   }
+
+  return request;
 };
-
-export async function request<T = any, K = any>(
-  input: RequestInfo,
-  init: RequestInitExtra<T, K> = {},
-): Promise<ResponseExtra<T> | K> {
-  if (!__sendRequest) {
-    throw new Error('Please initialize request first');
-  }
-  const requsetExtra = new $Request(input, init, __sendRequest);
-  await requsetExtra.setHeaders(headers);
-  await requsetExtra.parseBody();
-
-  const responseExtra = new $Response(await requsetExtra.flush(), init);
-  await responseExtra.parseBody();
-  await responseExtra.defineOk();
-
-  return responseExtra.flush();
-}
 
 export * from './helpers';
 export * from './types';
-
-export default request;
